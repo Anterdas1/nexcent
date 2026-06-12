567c723f4a13ff8488f83c722b878c87c40a0082
Anterdas1 <anterdas39039@gmail.com>
2026-06-12 12:38:12 +0600

    logo mistek

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,15 +15,12 @@ const Navbar = () => {
                         <ul
                             tabIndex="-1"
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                       <Link  href="/"> Home</Link>
+                       <Link  href="/"> Service</Link>
+                       <Link  href="/"> Feature</Link>
+                       <Link  href="/"> Product</Link>
+                       <Link  href="/"> Testimonial</Link>
+                       <Link  href="/"> FAQ</Link>
                         </ul>
                     </div>
                     <Image                    
